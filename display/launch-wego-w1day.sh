dfedb0eb1737f4c5d729068769d5bff8994eef98
unkode <monolith.contact@gmail.com>
2017-05-11 19:10:01 -0400

    Added hours in day forecast table

--- display/launch-wego-w1day.sh
+++ display/launch-wego-w1day.sh
@@ -3,6 +3,10 @@ do
   clear
   echo "Getting lastest meteorological data..."	
   #echo -n -e "$(~/display/wego/wego -d 1)"
-  echo -n -e "$(curl -s  -l "http://wttr.in/montreal?1&lang=fr&Q" | head -n -3)"
+  echo -n -e "$(curl -s  -l "http://wttr.in/montreal?1&lang=fr&Q" | head -n -3 | 
+  sed "s/             Matin            /          8h+ Matin           /g" |
+  sed "s/          Après-midi   /     12h+ Après-midi   /g" |
+  sed "s/      Soir            /   19h+ Soir          /g" |
+  sed "s/             Nuit             /          23h+ Nuit           /g")"
   sleep 2520
 done
--- display/launch-wego-wnow.sh
+++ display/launch-wego-wnow.sh
@@ -1,7 +1,7 @@
 while true
 do
   clear
-  echo -e "Dernière mise-à-jour:\r\n   $(date +%Y-%m-%d\ %H:%M:%S)\r\nMétéo actuelle:"
+  echo -e "Dernière mise-à-jour:\r\n   $(date +%Y-%m-%d\ %H:%M:%S)\r\nMétéo actuelle:\r\n"
   #echo -e "$(~/display/wego/wego -d 0 | grep -v "Weather for")"
   echo -e "$(curl -s  -l "http://wttr.in/montreal?0&lang=fr&Q")"
   echo -e -n "  ──────────────────────────"
